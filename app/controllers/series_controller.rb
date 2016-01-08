e131d323f55e166716d370f5dd1b77ad7718ddcd
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-01-08 10:20:14 -0500

    Encapsulate the grit in a model.

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,14 +1,5 @@
 class SeriesController < ApplicationController
   def index
-    series = Hash[
-      RSolr.connect(url: 'http://localhost:8983/solr/')
-           .get('select', params: {
-               'facet.field' => 'series_title',
-               'facet' => 'true',
-               'rows' => '0',
-               'f.series_title.facet.limit' => -1
-             }
-           )['facet_counts']['facet_fields']['series_title'].in_groups_of(2)]
-    @series_by_first_letter = series.each_pair.group_by{|pair| pair.first[0].upcase }
+    @series_by_first_letter = SeriesList.new.by_first_letter
   end
 end
---
+++ app/models/series_list.rb
@@ -0,0 +1,15 @@
+class SeriesList
+  def initialize(pairs = nil)
+    pairs ||= Hash[
+      RSolr.connect(url: 'http://localhost:8983/solr/')
+           .get('select', params: {
+               'facet.field' => 'series_title',
+               'facet' => 'true',
+               'rows' => '0',
+               'f.series_title.facet.limit' => -1
+             }
+           )['facet_counts']['facet_fields']['series_title'].in_groups_of(2)]
+    @by_first_letter = pairs.each_pair.group_by{ |pair| pair.first[0].upcase }
+  end
+  attr_reader :by_first_letter
+end
