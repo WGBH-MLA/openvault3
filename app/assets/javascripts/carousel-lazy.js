570a8ae73eb6304bf23f082d87219b0f4a9225dc
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-01-11 10:38:28 -0500

    Lazy load one ahead.

--- app/assets/javascripts/carousel-lazy.js
+++ app/assets/javascripts/carousel-lazy.js
@@ -1,6 +1,8 @@
 $(function(){
     $('body').on('slide.bs.carousel', function(e) {
-        var next_up = $(e.relatedTarget).find('img');
-        next_up.attr('src', next_up.attr('lazy-src'));
+        var $next_next_img = $(e.relatedTarget).next().find('img');
+        $next_next_img.attr('src', $next_next_img.attr('lazy-src'));
+        // No harm done if src is already set, or if we're at the end of the list.
+        // (If we only do the immediate "next", it hasn't actually loaded when the swap happens.) 
     }); 
 });
