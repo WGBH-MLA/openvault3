b32b7ded9ec1abb739bed2487ceee01bd9dbb490
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-01-13 20:06:32 -0500

    Collection base pages are no longer a thing.

--- spec/features/special_collections_spec.rb
+++ spec/features/special_collections_spec.rb
@@ -8,12 +8,6 @@ describe 'Special Collections' do
     expect(page).to have_text('The Advocates')
     expect_fuzzy_xml()
   end
-  it 'at least loads a details page base' do
-    visit '/collections/american-archive-of-public-broadcasting'
-    expect(page.status_code).to eq(200)
-    expect(page).to have_text('American Archive of Public Broadcasting')
-    expect_fuzzy_xml()
-  end
   it 'at least loads a details page tab' do
     visit '/collections/advocates-advocates/full-program-video'
     expect(page.status_code).to eq(200)
