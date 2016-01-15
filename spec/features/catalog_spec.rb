64a31d319ea2e13fd14f116c69a2c03f0aaf33a8
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-01-15 16:54:33 -0500

    Catalog#show test

--- spec/features/catalog_spec.rb
+++ spec/features/catalog_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 require_relative '../support/validation_helper'
 
 describe 'Catalog' do
+  before(:all) do
+    PBCoreIngester.load_fixtures
+  end
+  
   it 'at least loads the index page' do
     visit '/catalog'
     expect(page.status_code).to eq(200)
     expect_fuzzy_xml()
   end
-  xit 'at least loads a details page' do
-    visit '/catalog/1'
+  
+  it 'at least loads a details page' do
+    visit '/catalog/V_5FDB1545443B427888C90E7B15F3783A'
     expect(page.status_code).to eq(200)
     expect_fuzzy_xml()
   end
