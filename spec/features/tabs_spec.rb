2015d92c92eb8d199ebb1cc463e0fb669fabd811
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-01-26 20:18:37 -0500

    tabs page tests

---
+++ spec/features/tabs_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+require_relative '../support/validation_helper'
+
+describe 'Tab Pages' do
+  
+  {
+    exhibits: Exhibit.all,
+    collections: Collection.all
+  }.each do |top, all|
+    all.each do |tabbed|
+      describe tabbed.title do
+        (tabbed.tabs.keys - ['intro', 'extra']).each do |path|
+          "/#{top}/#{tabbed.path}/#{path}".tap do |target|
+            it "loads #{target}" do
+              visit target
+              expect(page.status_code).to eq(200)
+              expect_fuzzy_xml()
+            end
+          end
+        end
+      end
+    end
+  end
+  
+end
