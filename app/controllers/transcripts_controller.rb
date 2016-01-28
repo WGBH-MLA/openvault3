a715b705440ed74f7cad2b2b46d13b7ffb67c639
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-01-28 11:43:54 -0500

    Content for the iframe in something like the "right" way.

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -1,26 +1,19 @@
 class TranscriptsController < ApplicationController
   include Blacklight::Catalog
+  
+  layout 'plain'
     
   XSLT = Nokogiri::XSLT(File.read(__dir__+'/../../lib/xslt/tei_to_html.xsl'))
   
   def show
     # TODO: do we need more of the behavior from Blacklight::Catalog?
     @response, @document = fetch(params['id'])
-    xml = @document['xml']
-
-    respond_to do |format|
-      format.html do
-        @pbcore = PBCore.new(xml)
-        curl = Curl::Easy.http_get(@pbcore.transcript_src)
-        # S3 might do referer checks in the future
-        # curl.headers['Referer'] = 'http://openvault.wgbh.org/'
-        curl.perform
-        tei_doc = Nokogiri::XML(curl.body_str)
-        render text: '<html><body>' + XSLT.transform(tei_doc).to_xml + '</body></html>'
-        # TODO: Clean this up
-        # TODO: caching?
-      end
-    end
+    curl = Curl::Easy.http_get(PBCore.new(@document['xml']).transcript_src)
+    # S3 might do referer checks in the future
+    # curl.headers['Referer'] = 'http://openvault.wgbh.org/'
+    curl.perform
+    tei_doc = Nokogiri::XML(curl.body_str)
+    @transcript_html = XSLT.transform(tei_doc).to_xml
   end
   
 end
