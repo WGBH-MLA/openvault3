ae812052c1b25bb9157da0e6178575f3ef5dab88
Andrew Myers <afredmyers@gmail.com>
2016-02-05 16:10:15 -0500

    don't require pry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 require 'redirect_map'
-require 'pry'
 
 class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'pry'
 
 describe ApplicationController, type: :controller do
   describe '.redirect_map' do
