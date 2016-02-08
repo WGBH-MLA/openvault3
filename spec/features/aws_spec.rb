50d3525060dbff5ac4cbdcaaf64d82ef23aea3ca
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-02-08 13:55:06 -0500

    Supply referer when getting transcript.

--- spec/features/aws_spec.rb
+++ spec/features/aws_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 require 'curl'
 
 describe 'AWS' do
+  URL = 'https://s3.amazonaws.com/openvault.wgbh.org/catalog/asset_transcripts/A_00000000_MOCK.xml'
+
   describe 'good hosts' do
     ['localhost:3000', 'openvault.wgbh.org', 'demo.openvault.wgbh.org'].each do |host|
       it "loads from #{host}" do
-        curl = Curl::Easy.http_get('https://s3.amazonaws.com/openvault.wgbh.org/special_collections/advocates/advocates.png')
+        curl = Curl::Easy.http_get(URL)
         curl.headers['Referer'] = "http://#{host}/foo-bar"
         curl.perform
         expect(curl.status).to eq('200 OK')
@@ -16,7 +18,7 @@ describe 'AWS' do
   describe 'bad hosts' do
     ['localhost:3001', 'XYZ.openvault.wgbh.org', 'random.com'].each do |host|
       it "does not load from #{host}" do
-        curl = Curl::Easy.http_get('https://s3.amazonaws.com/openvault.wgbh.org/special_collections/advocates/advocates.png')
+        curl = Curl::Easy.http_get(URL)
         curl.headers['Referer'] = "http://#{host}/foo-bar"
         curl.perform
         expect(curl.status).to eq('403 Forbidden')
@@ -26,7 +28,7 @@ describe 'AWS' do
 
   describe 'missing referer' do
     it 'does not load w/o referer' do
-      curl = Curl::Easy.http_get('https://s3.amazonaws.com/openvault.wgbh.org/special_collections/advocates/advocates.png')
+      curl = Curl::Easy.http_get(URL)
       curl.perform
       expect(curl.status).to eq('403 Forbidden')
     end
