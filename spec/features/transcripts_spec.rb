3c88f84a34496b757fd2dc90c8dc87fb98b2fcb6
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-02-09 20:09:43 -0500

    And test that we proxy the xml. (And also just a sanity check so we can tell if the remote xml changed.)

--- spec/features/transcripts_spec.rb
+++ spec/features/transcripts_spec.rb
@@ -7,9 +7,16 @@ describe 'Transcripts' do
     PBCoreIngester.load_fixtures
   end
 
-  it 'at least loads a details page' do
+  it 'proxies the XML' do
+    visit '/transcripts/A_00000000_MOCK.xml'
+    expect(page.status_code).to eq(200)
+    expect(page.body).to eq(File.read(Rails.root+'spec/fixtures/transcript/mock-transcript.xml'))
+  end
+  
+  it 'does the right XSLT' do
     visit '/transcripts/A_00000000_MOCK'
     expect(page.status_code).to eq(200)
     expect(page.body).to eq(File.read(Rails.root+'spec/fixtures/transcript/mock-transcript.html'))
   end
+  
 end
