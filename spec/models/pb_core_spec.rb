4fb08265d547c8f899368b6ec748175f85abff3b
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-02-15 16:47:16 -0500

    fix pb_core_spec

--- spec/models/pb_core_spec.rb
+++ spec/models/pb_core_spec.rb
@@ -114,6 +114,7 @@ describe 'Validated and plain PBCore' do
         video?: true,
         audio?: false,
         image?: false,
+        digitized?: true,
         access: ['All Records', 'Available Online'],
         subjects: ['SUBJECT-1', 'SUBJECT-2'],
         genres: ['GENRE-1', 'GENRE-2'],
