9c5b26f261b4736514bdad3d8a33e3ddcea440c9
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-02-15 11:05:09 -0500

    Respect double line breaks in source data. Towards #176... but keep open till source data is correct.

--- spec/features/catalog_spec.rb
+++ spec/features/catalog_spec.rb
@@ -7,69 +7,74 @@ describe 'Catalog' do
     PBCoreIngester.load_fixtures
   end
 
-  it 'loads the index page' do
-    visit '/catalog'
-    expect(page.status_code).to eq(200)
-    expect_fuzzy_xml
+  describe '#index' do
+    it 'loads the index page' do
+      visit '/catalog'
+      expect(page.status_code).to eq(200)
+      expect_fuzzy_xml
+    end
+
+    it 'redirects missing access' do
+      visit '/catalog?q='
+      expect(page.status_code).to eq(200)
+      expect(URI.parse(current_url).query).to eq('f[access][]=Available+Online&q=')
+    end
+
+    it 'redirects missing everything' do
+      visit '/catalog?'
+      expect(page.status_code).to eq(200)
+      expect(URI.parse(current_url).query).to eq('f[access][]=Available+Online')
+    end
+
+    it 'has a helpful no-results' do
+      visit '/catalog?q=asdfasfasdf'
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content 'Consider using other search terms, removing filters, or searching all records, not just those with media.'
+      expect_fuzzy_xml
+    end
+
+    it 'also has an unhelpful no-results' do
+      visit '/catalog?q=asdfasfasdf&f[access][]=All+Records'
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content 'Consider using other search terms or removing filters.'
+      expect_fuzzy_xml
+    end
+
+    it 'sorts by title ascending, ignoring articles' do
+      visit '/catalog?f[access][]=All+Records&q=sort&sort=title+asc'
+      expect(page.status_code).to eq(200)
+      expect_fuzzy_xml
+      expect(page.all('.info').map(&:text)).to eq([
+        '"The" removed: sort 1',
+        '"An" REMOVED: SORT 2',
+        '"A" removed: sort 3'
+      ])
+    end
+
+    it 'sorts by title descending, ignoring articles' do
+      visit '/catalog?f[access][]=All+Records&q=sort&sort=title+desc'
+      expect(page.status_code).to eq(200)
+      expect_fuzzy_xml
+      expect(page.all('.info').map(&:text)).to eq([
+        '"A" removed: sort 3',
+        '"An" REMOVED: SORT 2',
+        '"The" removed: sort 1'
+      ])
+    end
   end
 
-  it 'redirects missing access' do
-    visit '/catalog?q='
-    expect(page.status_code).to eq(200)
-    expect(URI.parse(current_url).query).to eq('f[access][]=Available+Online&q=')
-  end
+  describe '#show' do
+    it 'loads a full details page' do
+      visit '/catalog/A_00000000_MOCK'
+      expect(page.status_code).to eq(200)
+      expect_fuzzy_xml
+    end
 
-  it 'redirects missing everything' do
-    visit '/catalog?'
-    expect(page.status_code).to eq(200)
-    expect(URI.parse(current_url).query).to eq('f[access][]=Available+Online')
-  end
-
-  it 'has a helpful no-results' do
-    visit '/catalog?q=asdfasfasdf'
-    expect(page.status_code).to eq(200)
-    expect(page).to have_content 'Consider using other search terms, removing filters, or searching all records, not just those with media.'
-    expect_fuzzy_xml
-  end
-
-  it 'also has an unhelpful no-results' do
-    visit '/catalog?q=asdfasfasdf&f[access][]=All+Records'
-    expect(page.status_code).to eq(200)
-    expect(page).to have_content 'Consider using other search terms or removing filters.'
-    expect_fuzzy_xml
-  end
-
-  it 'loads a full details page' do
-    visit '/catalog/A_00000000_MOCK'
-    expect(page.status_code).to eq(200)
-    expect_fuzzy_xml
-  end
-
-  it 'loads a minimal details page' do
-    visit '/catalog/A_00B0C50853C64A71935737EF7A4DA66C'
-    expect(page.status_code).to eq(200)
-    expect_fuzzy_xml
-  end
-
-  it 'sorts by title ascending, ignoring articles' do
-    visit '/catalog?f[access][]=All+Records&q=sort&sort=title+asc'
-    expect(page.status_code).to eq(200)
-    expect_fuzzy_xml
-    expect(page.all('.info').map(&:text)).to eq([
-      '"The" removed: sort 1',
-      '"An" REMOVED: SORT 2',
-      '"A" removed: sort 3'
-    ])
-  end
-
-  it 'sorts by title descending, ignoring articles' do
-    visit '/catalog?f[access][]=All+Records&q=sort&sort=title+desc'
-    expect(page.status_code).to eq(200)
-    expect_fuzzy_xml
-    expect(page.all('.info').map(&:text)).to eq([
-      '"A" removed: sort 3',
-      '"An" REMOVED: SORT 2',
-      '"The" removed: sort 1'
-    ])
+    it 'loads a minimal details page' do
+      visit '/catalog/A_00B0C50853C64A71935737EF7A4DA66C'
+      expect(page.status_code).to eq(200)
+      expect_fuzzy_xml
+      expect(page.html).to match(/<p>Line breaks\s+do not matter, but<\/p><p>empty lines do\.<\/p>/)
+    end
   end
 end
