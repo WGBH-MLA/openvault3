3a992b28d1abc52aacfadcf41476b99009cd00e3
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-03-01 09:55:09 -0500

    whitespace.

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -3,13 +3,14 @@ class RobotsController < ApplicationController
   def show
     respond_to do |format|
       format.txt do
-        render text: if request.host == REAL_HOST
-                       <<EOF
+        render text:
+          if request.host == REAL_HOST
+            <<EOF
 User-agent: *
 Disallow: /catalog?
 EOF
-                     else
-                       <<EOF
+          else
+            <<EOF
 User-agent: *
 Disallow: /
 # Only #{REAL_HOST} should be indexed.
