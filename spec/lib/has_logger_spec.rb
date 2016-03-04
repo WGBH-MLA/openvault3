f730f64e672ae1c2b2ba5a9042c3a7c31060287a
Andrew Myers <afredmyers@gmail.com>
2016-03-04 12:58:14 -0500

    Adds HasLogger module
    Exposes an enpoint to a Logger at #logger.
    Default logger logs to stdout.
    Allows re-assigning logger to a different Logger object with #set_logger.

---
+++ spec/lib/has_logger_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require 'has_logger'
+
+describe HasLogger do
+  subject do
+    test_class = Class.new do
+      include HasLogger
+    end
+    test_class.new
+  end
+
+  describe 'logger' do
+    it 'returns a Logger object' do
+      expect(subject.logger).to be_a Logger
+    end
+  end
+
+  describe '#set_logger' do
+    context 'when given something that is not a Logger object' do
+      it 'raises an InvalidLogger error' do
+        expect{ subject.set_logger "not a logger" }.to raise_error HasLogger::InvalidLogger
+      end
+    end
+  end
+end
