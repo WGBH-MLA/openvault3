ad931a8fc74a21227b26e45685074950959a4f05
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-03-07 15:19:38 -0500

    bare-bones sitemap.

---
+++ app/controllers/sitemap_controller.rb
@@ -0,0 +1,15 @@
+class SitemapController < ApplicationController
+  ALL_IDS = RSolr.connect(url: 'http://localhost:8983/solr/')
+            .get('select', params: {
+                   'q' => '*:*',
+                   'fl' => 'id',
+                   'rows' => '50000' # Max for sitemaps
+                 })['response']['docs'].map { |d| d['id'] }
+  def index
+    respond_to do |format|
+      format.xml do
+        render
+      end
+    end
+  end
+end
