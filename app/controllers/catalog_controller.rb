17cc298505b14acb44bd6decd8a20c238ed86024
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-04-06 15:38:28 -0400

    Search terms in context on hover.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -19,7 +19,7 @@ class CatalogController < ApplicationController
     config.default_solr_params = {
       qt: 'search',
       rows: 12,
-      'hl.fl': 'text',
+      'hl.fl' => 'text',
       hl: true
     }
 
