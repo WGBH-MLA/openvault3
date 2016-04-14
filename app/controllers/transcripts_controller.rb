dc20eddedbe20a525f9a4e1a92e4c793fe5b3f92
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-04-14 16:59:23 -0400

    Pull logic out into separate class.

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -4,7 +4,6 @@ class TranscriptsController < ApplicationController
   layout 'transcript'
 
   HTML_XSLT = Nokogiri::XSLT(File.read(__dir__ + '/../../lib/xslt/tei_to_html.xsl'))
-  VTT_XSLT = Nokogiri::XSLT(File.read(__dir__ + '/../../lib/xslt/tei_to_vtt.xsl'))
 
   caches_page :show
   def show
@@ -23,11 +22,7 @@ class TranscriptsController < ApplicationController
         render
       end
       format.vtt do
-        tei_doc = Nokogiri::XML(curl.body_str)
-        ugly_vtt = VTT_XSLT.transform(tei_doc).to_s
-                   .sub('<?xml version="1.0" encoding="utf-8"?>' + "\n", '').gsub('&gt;', '>') # Bugs in Nokogiri?
-                   .gsub(/(\D)\s*\n\s*(\D)/, '\1 \2') # remove newlines, and indentation, but only from text blocks
-        render text: "WEBVTT FILE\n\n" + ugly_vtt
+        render text: Captioner.from_tei(curl.body_str)
       end
       format.xml do
         render text: curl.body_str
---
+++ lib/captioner.rb
@@ -0,0 +1,12 @@
+class Captioner
+  VTT_XSLT = Nokogiri::XSLT(File.read(__dir__ + '/xslt/tei_to_vtt.xsl'))
+  
+  def self.from_tei(tei_xml)
+    tei_doc = Nokogiri::XML(tei_xml)
+    ugly_vtt = VTT_XSLT.transform(tei_doc).to_s
+                   .sub('<?xml version="1.0" encoding="utf-8"?>' + "\n", '').gsub('&gt;', '>') # Bugs in Nokogiri?
+                   .gsub(/(\D)\s*\n\s*(\D)/, '\1 \2') # remove newlines, and indentation, but only from text blocks
+    "WEBVTT FILE\n\n" + ugly_vtt
+  end
+  
+end
