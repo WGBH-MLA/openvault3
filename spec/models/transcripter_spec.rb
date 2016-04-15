83fe3daa82ca9889bc3d90a841bcb9020ce93a91
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-04-15 13:45:53 -0400

    wrap

--- spec/models/transcripter_spec.rb
+++ spec/models/transcripter_spec.rb
@@ -1,8 +1,10 @@
 require_relative '../../lib/transcripter'
 
 describe Transcripter do
-  it 'produces HTML' do
-    tei = File.read(__dir__ + '/../fixtures/transcript/mock-transcript.xml')
-    expect(Transcripter.from_tei(tei)).to eq(File.read(__dir__ + '/../fixtures/transcript/mock-transcript.html'))
+  describe '#from_tei' do
+    it 'produces HTML' do
+      tei = File.read(__dir__ + '/../fixtures/transcript/mock-transcript.xml')
+      expect(Transcripter.from_tei(tei)).to eq(File.read(__dir__ + '/../fixtures/transcript/mock-transcript.html'))
+    end
   end
 end
