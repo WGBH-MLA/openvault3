b593980a7a6d7f9f74b1a8a9574aac919dff1360
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-06-07 11:38:15 -0400

    Tweak tests to reflect current policy. Fix #631

--- spec/aws/s3_spec.rb
+++ spec/aws/s3_spec.rb
@@ -1,14 +1,29 @@
 require 'aws-sdk'
 require 'json'
+require 'curl'
 
-describe 'S3', not_on_travis: true do
-  def to_pretty_json(string_io)
-    JSON.pretty_generate(JSON.parse(string_io.string))
+describe 'S3' do
+  describe 'policy implementation' , not_on_travis: true do
+    def to_pretty_json(string_io)
+      JSON.pretty_generate(JSON.parse(string_io.string))
+    end
+    let(:client) { Aws::S3::Client.new(region: 'us-east-1') }
+    it 'has expected policy' do
+      expect(
+        to_pretty_json(client.get_bucket_policy(bucket: 'openvault.wgbh.org').policy)
+      ).to eq(File.read(__dir__ + '/../fixtures/aws/bucket-policy.json'))
+    end
   end
-  let(:client) { Aws::S3::Client.new(region: 'us-east-1') }
-  it 'has expected policy' do
-    expect(
-      to_pretty_json(client.get_bucket_policy(bucket: 'openvault.wgbh.org').policy)
-    ).to eq(File.read(__dir__ + '/../fixtures/aws/bucket-policy.json'))
+  describe 'policy effect' do
+    it 'allows thumbnail without referer' do
+      curl = Curl::Easy.http_get('https://s3.amazonaws.com/openvault.wgbh.org/catalog/asset_thumbnails/V_E7B307B7ACAF4A89B41CFD03EF68630B.jpg')
+      curl.perform
+      expect(curl.status).to eq('200 OK')
+    end
+    it 'disallows proxies without referer' do
+      curl = Curl::Easy.http_get('https://s3.amazonaws.com/openvault.wgbh.org/catalog/asset_proxies/A_00000000_MOCK.mp3')
+      curl.perform
+      expect(curl.status).to eq('403 Forbidden')
+    end
   end
 end
