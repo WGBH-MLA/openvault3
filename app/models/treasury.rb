bd95abb1e89789f8af07693ac8b2916dd3e1e604
foglabs <foglaboratories@gmail.com>
2020-09-11 18:04:03 -0400

    'cache bust'

--- app/models/treasury.rb
+++ app/models/treasury.rb
@@ -4,7 +4,7 @@ class Treasury
   attr_reader :data
 
   def self.xml_docs
-    Rails.cache.fetch("cooke_xml_records_4") do
+    Rails.cache.fetch("cooke_xml_records_5") do
       # sort all cooke records by date, as xml, so it FAST
       RSolr.connect(url: 'http://localhost:8983/solr/').get('select', params: {'q' => "special_collections:alistair-cooke", 'fl' => 'xml', 'rows' => '1000'})['response']['docs'].map {|doc| doc['xml'] }.sort_by {|xml| Treasury.broadcast_date_from_xml(xml) }
     end
@@ -12,7 +12,7 @@ class Treasury
 
   def self.generate_list_seasons
     # this takes... a long time
-    Rails.cache.fetch("cooke_list_4") do
+    Rails.cache.fetch("cooke_list_5") do
       @list_seasons = []
 
       # a data structure only a mother could love
